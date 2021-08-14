62e5e91f7970b7ba46b25a7d16a502c24337f73f
AndroPlus-org <hosigumayuugi@gmail.com>
2021-08-14 21:01:30 +0900

    ダウンロード先を追記

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ unzip roboto.zip
 cd ../
 
 echo "Mejiro を生成するには FontForge をインストールし、"
-echo "最新の Mplus 1を mplus/ に展開した後、以下を実行してください"
+echo "最新の mplus-TESTFLIGHT-*.tar.xz を"
+echo "https://ja.osdn.net/projects/mplus-fonts/releases/62344"
+echo "からダウンロードして mplus/ に展開した後、以下を実行してください"
 echo "fontforge -lang=py -script mejiro.py"
 tput sgr0
